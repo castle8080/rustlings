7a7e4f8ad1e9981df99df291dd7c78adc88fd0a3
Bryan Castillo <bryanc@microsoft.com>
2023-01-17 11:57:23 -0800

    Doing a few more exercises.

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -25,9 +23,25 @@ struct Team {
     goals_conceded: u8,
 }
 
+fn update_team_score(previous: Option<&Team>, team_name: &String, goals_scored: u8, goals_conceded: u8) -> Team {
+    match previous {
+        None => Team {
+            name: team_name.to_string(),
+            goals_scored: goals_scored,
+            goals_conceded: goals_conceded
+        },
+        Some(entry) => Team {
+            name: team_name.to_string(),
+            goals_scored: entry.goals_scored + goals_scored,
+            goals_conceded: entry.goals_conceded + goals_conceded
+        }
+    }
+}
+
 fn build_scores_table(results: String) -> HashMap<String, Team> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
+
 
     for r in results.lines() {
         let v: Vec<&str> = r.split(',').collect();
@@ -35,11 +49,12 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+
+        let team1_info = update_team_score(scores.get(&team_1_name), &team_1_name, team_1_score, team_2_score);
+        let team2_info = update_team_score(scores.get(&team_2_name), &team_2_name, team_2_score, team_1_score);
+
+        scores.insert(team_1_name, team1_info);
+        scores.insert(team_2_name, team2_info);
     }
     scores
 }
